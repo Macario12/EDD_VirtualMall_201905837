18c6e94ea1dc617df43dda81c1f1353c088f1721
Macario12 <arielmacario.11@gmail.com>
2021-02-09 01:06:27 -0600

    Servidor y Puerto 300

--- main.go
+++ main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
-	"./estructura"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
 	"./tienda"
+	"github.com/gorilla/mux"
 )
+
+type vector struct {
+	Datos []datos `json:"Datos"`
+}
+
+type datos struct {
+	indice        string          `json:"Indice"`
+	Departamentos []departamentos `json:"Departamentos"`
+}
+
+type departamentos struct {
+	Departamento string         `json:"Nombre"`
+	Tiendas      []tienda.Store `json:"Tiendas"`
+}
+
+func agregar(w http.ResponseWriter, r *http.Request) {
+	var ms vector
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error al insertar")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.Unmarshal(reqBody, &ms)
+
+	fmt.Println(ms.Datos)
+
+	json.NewEncoder(w).Encode(ms)
+}
+
+func inicial(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Funcionan EDD")
+}
 
 func main() {
 
-	tiendaprueba := tienda.Store{"Hola", "holaputa", "124537", 5}
-	tiendaprueba2 := tienda.Store{"Hola2", "holaputito", "124537", 5}
-	tiendaprueba3 := tienda.Store{"Tienda1", "hola", "124537", 5}
-	tiendaprueba4 := tienda.Store{"Tienda2", "hola", "124537", 5}
-
-	lista := estructura.List{}
-
-	lista.Add(tiendaprueba)
-	lista.Add(tiendaprueba2)
-	lista.Add(tiendaprueba3)
-	lista.Add(tiendaprueba4)
-	lista.Printlist()
-	lista.DeleteStore("Tienda1")
-	lista.Printlist()
+	router := mux.NewRouter()
+	router.HandleFunc("/", inicial).Methods("GET")
+	router.HandleFunc("/agregar", agregar).Methods("POST")
+	log.Fatal(http.ListenAndServe(":3000", router))
 
 }
